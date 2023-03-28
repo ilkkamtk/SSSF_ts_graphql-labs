cd7c3a08ab53a415969ba1b5473cc4ee7d420c4b
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-28 09:54:54 +0300

    database started

--- src/api/resolvers/animalResolver.ts
+++ src/api/resolvers/animalResolver.ts
@@ -1,42 +1,10 @@
 import {Animal} from '../../interfaces/Animal';
-const animalData = [
-  {
-    id: '1',
-    animal_name: 'Frank',
-    species: '1',
-  },
-];
-
-const speciesData = [
-  {
-    id: '1',
-    species_name: 'Cat',
-    category: '1',
-  },
-];
-
-const categoryData = [
-  {
-    id: '1',
-    category_name: 'Mammal',
-  },
-];
+import animalModel from '../models/animalModel';
 
 export default {
   Query: {
-    animals: (_parent: undefined, _args: Animal) => {
-      return animalData;
-    },
-  },
-  Animal: {
-    species: (parent: any) => {
-      console.log(parent);
-      return speciesData.find((species) => species.id === parent.species);
-    },
-  },
-  Species: {
-    category: (parent: any) => {
-      return categoryData.find((category) => category.id === parent.category);
+    animals: async () => {
+      return await animalModel.find();
     },
   },
 };
--- src/api/resolvers/speciesResolver.ts
+++ src/api/resolvers/speciesResolver.ts
@@ -1,1 +1,11 @@
-// TODO: speciesResolver
+import {Animal} from '../../interfaces/Animal';
+import speciesModel from '../models/speciesModel';
+
+export default {
+  Animal: {
+    species: async (parent: Animal) => {
+      console.log(parent);
+      return await speciesModel.findById(parent.species);
+    },
+  },
+};
